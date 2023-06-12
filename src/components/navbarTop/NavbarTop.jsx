4b5519d4d2244f8e533e63531974a8e52f759f32
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2023-06-12 14:38:35 +0530

    making navbartop

--- src/components/navbarTop/NavbarTop.jsx
+++ src/components/navbarTop/NavbarTop.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
+import React from "react";
 import "./navbarTop.scss";
+import {
+  MdLocationOn,
+  MdOutlinePersonOutline,
+  MdOutlineLanguage,
+} from "react-icons/md";
+import { WiDayCloudy } from "react-icons/wi";
+import { BiCalendar } from "react-icons/bi";
+import { RxCaretDown } from "react-icons/rx";
+import { RiFacebookFill, RiTwitterFill } from "react-icons/ri";
+import { ImGooglePlus } from "react-icons/im";
+import { MdRssFeed } from "react-icons/md";
+import { FaYoutube } from "react-icons/fa";
 
 const NavbarTop = () => {
   return (
     <div className="navbarTop">
-        <div className="navbarContainer">navbar</div>
+      <div className="navbarContainer">
+        <div className="left">
+          <div className="item">
+            <MdLocationOn style={{ fontSize: "16px" }} />
+            <span>New York</span>
+          </div>
+          <div className="item">
+            <WiDayCloudy style={{ fontSize: "16px" }} />
+            <span>21°C</span>
+          </div>
+          <div className="item">
+            <BiCalendar style={{ fontSize: "16px" }} />
+            <span>Today</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="item">
+            <MdOutlinePersonOutline style={{ fontsize: "16px" }} />
+            <span>Login/Register</span>
+          </div>
+          <div className="item">
+            <MdOutlineLanguage style={{ fontsize: "16px" }} />
+            <span>English</span>
+            <RxCaretDown style={{ fontSize: "16px" }} />
+          </div>
+          <div className="icons">
+            <RiFacebookFill style={{ fontSize: "16px" }} />
+            <RiTwitterFill style={{ fontSize: "16px" }} />
+            <ImGooglePlus style={{ fontSize: "16px" }} />
+            <MdRssFeed style={{ fontSize: "16px" }} />
+            <FaYoutube style={{ fontSize: "16px" }} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default NavbarTop
+export default NavbarTop;
