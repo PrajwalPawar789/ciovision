f2ee7fa657dc366e8cdb97e60a2bf7c9e1836023
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2023-06-13 08:19:26 +0530

    Completed AdvertTopbar

---
+++ src/components/advertTop/AdvertTop.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./advertTop.scss";
+
+const AdvertTop = () => {
+  return (
+    <div className="advertTop">
+    <div className="advertTopContainer">
+        <div className="left">
+            <a>
+                <img src="assets\assets\logo.png" alt="US News Logo"/>
+            </a>
+        </div>
+        <div className="right">
+            <a>
+               <img src="assets\assets\advertttt.gif" alt="Advert git"/>
+            </a>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default AdvertTop
