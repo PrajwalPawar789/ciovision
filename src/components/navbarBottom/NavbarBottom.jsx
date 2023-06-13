6755e8ce5c5591614a311905cbc481815b89ea35
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2023-06-13 08:20:21 +0530

    building navbarBottom

---
+++ src/components/navbarBottom/NavbarBottom.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavbarBottom = () => {
+  return (
+    <div className="navbarBottom">
+        <div className="navbarBottomWrapper">
+            <div className="item">
+                
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default NavbarBottom
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./home.scss";
+import AdvertTop from "../../components/advertTop/AdvertTop.jsx";
 import NavbarTop from "../../components/navbarTop/NavbarTop.jsx";
+import NavbarBottom from "../../components/navbarBottom/NavbarBottom.jsx";
 
 const Home = () => {
   return (
     <div className="homeWrapper">
       <NavbarTop/>
+      <AdvertTop />
+      <NavbarBottom/>
     </div>
   )
 }
