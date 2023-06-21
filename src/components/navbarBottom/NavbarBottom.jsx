83535aedd5ca5a2dc53a59a4cb71960bb6212fcf
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2023-06-21 08:59:34 +0530

    now just compledted navbarbottom

--- src/components/navbarBottom/NavbarBottom.jsx
+++ src/components/navbarBottom/NavbarBottom.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import "./navbarBottom.scss";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { FaSearch} from "react-icons/fa";
 
 const NavbarBottom = () => {
   return (
     <div className="navbarBottom">
         <div className="navbarBottomWrapper">
             <div className="item">
-                
+                <span>Home</span>
+                <MdKeyboardArrowDown style={{ fontSize: "20px"}}/>
+            </div>
+            <div className="item">
+                <span>Worlds News</span>
+                <MdKeyboardArrowDown style={{ fontSize: "20px"}}/>
+            </div>
+            <div className="item">
+                <span>National</span>
+            </div>
+            <div className="item">
+                <span>Financial</span>
+            </div>
+            <div className="item">
+                <span>Enterntainment</span>
+            </div>
+            <div className="item">
+                <span>LifeStyle</span>
+            </div>
+            <div className="item">
+                <span>Technology</span>
+            </div>
+            <div className="item">
+                <span>Travel</span>
+                <MdKeyboardArrowDown style={{ fontSize: "20px"}}/>
+            </div>
+            <div className="item">
+                <span>Catagory</span>
+                <MdKeyboardArrowDown style={{ fontSize: "20px"}}/>
+            </div>
+            <div className="item">
+                <span>Pages</span>
+                <MdKeyboardArrowDown style={{ fontSize: "20px"}}/>
+            </div>
+            <div className="item">
+              <FaSearch style= {{fontSize: "15px"}}/>
             </div>
         </div>
     </div>
